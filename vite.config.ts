656df4caa838a0f19256e8bbe1c7e3a5b947ae0e
Criss Sanz <krissanz13@gmail.com>
2024-10-17 00:23:35 +0200

    #fix - Fixing tsconfig

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+
+import { fileURLToPath, URL } from 'url';
 import { defineConfig } from 'vite';
 
 export default defineConfig(({ mode }) => {
@@ -8,20 +9,56 @@ export default defineConfig(({ mode }) => {
   return {
     plugins: [react()],
     resolve: {
-      alias: {
-        '@/': path.resolve(__dirname, './src'),
-        '@/adapters': path.resolve(__dirname, './src/components'),
-        '@/pages': path.resolve(__dirname, './src/pages'),
-        '@/utils': path.resolve(__dirname, './src/utils'),
-        '@/assets': path.resolve(__dirname, './src/assets'),
-        '@/redux': path.resolve(__dirname, './src/redux'),
-        '@/services': path.resolve(__dirname, './src/services'),
-        '@/routes': path.resolve(__dirname, './src/routes'),
-        '@/models': path.resolve(__dirname, './src/models'),
-        '@/hooks': path.resolve(__dirname, './src/hooks'),
-        '@/components': path.resolve(__dirname, './src/components'),
-        '@/modules': path.resolve(__dirname, './src/modules'),
-      },
+      alias: [
+        {
+          find: '@/',
+          replacement: fileURLToPath(new URL('./src', import.meta.url)),
+        },
+        {
+          find: '@/adapters',
+          replacement: fileURLToPath(new URL('./src/components', import.meta.url)),
+        },
+        {
+          find: '@/pages',
+          replacement: fileURLToPath(new URL('./src/pages', import.meta.url)),
+        },
+        {
+          find: '@/utils',
+          replacement: fileURLToPath(new URL('./src/utils', import.meta.url)),
+        },
+        {
+          find: '@/assets',
+          replacement: fileURLToPath(new URL('./src/assets', import.meta.url)),
+        },
+        {
+          find: '@/redux',
+          replacement: fileURLToPath(new URL('./src/redux', import.meta.url)),
+        },
+        {
+          find: '@/services',
+          replacement: fileURLToPath(new URL('./src/services', import.meta.url)),
+        },
+        {
+          find: '@/routes',
+          replacement: fileURLToPath(new URL('./src/routes', import.meta.url)),
+        },
+        {
+          find: '@/models',
+          replacement: fileURLToPath(new URL('./src/models', import.meta.url)),
+        },
+        {
+          find: '@/hooks',
+          replacement: fileURLToPath(new URL('./src/hooks', import.meta.url)),
+        },
+        {
+          find: '@/components',
+          replacement: fileURLToPath(new URL('./src/components', import.meta.url)),
+        },
+        {
+          find: '@/modules',
+          replacement: fileURLToPath(new URL('./src/modules', import.meta.url)),
+        },
+      ],
     },
     build: {
       minify: isProduction ? 'esbuild' : false, // Minify in production, not in development
